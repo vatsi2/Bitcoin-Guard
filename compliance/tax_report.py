56e76ef7f7cb7f49c29ef095a2e6b88c53269a25
youliang <161660584+vatsi2@users.noreply.github.com>
2025-03-21 02:44:43 +0100

    Delete compliance directory

--- compliance/tax_report.py
+++
@@ -1,9 +0,0 @@
-from defiguard import FIFOAnalyzer, TaxLotTracker
-
-class CapitalGainsReport:
-    def __init__(self, wallet: str, jurisdiction: str):
-        self.tracker = TaxLotTracker(wallet)
-        self.analyzer = FIFOAnalyzer(jurisdiction)
-        
-    def generate_report(self) -> pd.DataFrame:
-        return self.analyzer.process(self.tracker.transactions)
