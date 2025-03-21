2411b69fb1d08bd28bfcd7a4fdbae5a6a347cf30
youliang <161660584+vatsi2@users.noreply.github.com>
2025-03-21 02:44:58 +0100

    Delete core directory

--- core/tax_harvester.py
+++
@@ -1,6 +0,0 @@
-class LossHarvester:
-    def execute_tax_loss(self, position):
-        if position.unrealized_loss > 1000:  # $1000 threshold
-            if not self.wash_sale_check(position.asset):
-                self.exchange.sell(position.asset, position.amount)
-                self.schedule_rebuy(position.asset)
