285bbfb8bdbf563884553857f5d706f8d839484a
youliang <161660584+vatsi2@users.noreply.github.com>
2025-03-21 02:45:37 +0100

    Delete tests directory

--- tests/integration_tests.rs
+++
@@ -1,9 +0,0 @@
-#[cfg(test)]
-mod tests {
-    #[tokio::test]
-    async fn test_liquidation_protection() {
-        let shield = LiquidationShield::new("0x...");
-        shield.check_position();
-        // Assert collateral adjustment
-    }
-}
