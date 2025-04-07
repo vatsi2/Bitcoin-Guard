73e6db8a44119c24ca79eb079909a7e6c7bdf475
youliang <161660584+vatsi2@users.noreply.github.com>
2025-04-07 09:14:25 +0100

    Delete main directory

--- main/aes-gcm.rs
+++
@@ -1,9 +0,0 @@
-use aes_gcm::{Aes256Gcm, KeyInit, aead::{Aead, OsRng}};
-use aes_gcm::aead::generic_array::GenericArray;
-
-fn encrypt_key(plaintext: &[u8], password: &str) -> Vec<u8> {
-    let key = GenericArray::from_slice(password.as_bytes());
-    let cipher = Aes256Gcm::new(key);
-    let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
-    cipher.encrypt(&nonce, plaintext).unwrap()
-}
