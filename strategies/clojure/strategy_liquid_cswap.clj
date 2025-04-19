744f0c3bcf6ee623f52b569fea78f1790617e6a5
youliang <161660584+vatsi2@users.noreply.github.com>
2025-04-19 23:33:52 +0100

    Create strategy_liquid_cswap.clj

---
+++ strategies/clojure/strategy_liquid_cswap.clj
@@ -0,0 +1,8 @@
+;; Strategy: Liquid CSwap Bot
+;; Language: Clojure
+;; Project: Liquid Network CSwap
+
+(ns hodldesk.strategy
+  (:require [bitcore.rpc :as rpc]))
+
+;; TODO: arbitrage across Liquid CSwap markets
