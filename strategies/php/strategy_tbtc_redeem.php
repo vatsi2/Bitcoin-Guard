3dc3c9a7f84ca2be0b7c2a4d5443a7d182056db9
youliang <161660584+vatsi2@users.noreply.github.com>
2025-04-19 23:32:32 +0100

    Create strategy_tbtc_redeem.php

---
+++ strategies/php/strategy_tbtc_redeem.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Strategy: tBTC Redeemer
+ * Language: PHP
+ * Project: tBTC
+ */
+$rpc = new JsonRPC\Client('http://user:pass@127.0.0.1:8332');
+// TODO: monitor tBTC redemptions, trigger BTC withdrawals
