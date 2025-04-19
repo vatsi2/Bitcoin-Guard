132fe1596b3e6ece5371d9e72b6f4aa35be123f2
youliang <161660584+vatsi2@users.noreply.github.com>
2025-04-19 23:33:40 +0100

    Create Strategy_lightning_fee.scala

---
+++ strategies/scala/Strategy_lightning_fee.scala
@@ -0,0 +1,7 @@
+// Strategy: Lightning Fee Optimization
+// Language: Scala
+// Project: Lightning Network
+
+object Strategy_lightning_fee extends App {
+  // TODO: fetch channel stats, optimize fee schedules
+}
